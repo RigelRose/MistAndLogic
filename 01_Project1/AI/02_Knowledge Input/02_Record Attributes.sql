090244424e00087bc57350fc8b4a259b515679aa
Rigel Rosé <joylinedsouza78@gmail.com>
2025-10-21 18:58:13 +0530

    Create 02_Record Attributes.sql

---
+++ 01_Project1/AI/02_Knowledge Input/02_Record Attributes.sql
@@ -0,0 +1,7 @@
+OpenInvoiceDeepdiveManualFile    id : O_CUSTOM_OPENINVOICEDEEPDIVEMANUALFILE    pql : "o_custom_OpenInvoiceDeepdiveManualFile"
+
+Companycode :  Company Code is a unique identifier used to represent an independent legal accounting entity within an organization. It defines the smallest unit for which financial statements such as balance sheets and profit & loss statements can be created.
+pql query :  "o_custom_OpenInvoiceDeepdiveManualFile"."COMPANYCODE"
+
+Exception Type : 
+pql query  :  "o_custom_OpenInvoiceDeepdiveManualFile"."ExceptionType"
