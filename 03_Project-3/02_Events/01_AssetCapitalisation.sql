d5e5b38294ae1e5ce43191144ac5fd0c2e676d0f
Rigel Rosé <joylinedsouza78@gmail.com>
2025-10-25 11:55:18 +0530

    Create 01_AssetCapitalisation.sql

---
+++ 03_Project-3/02_Events/01_AssetCapitalisation.sql
@@ -0,0 +1,6 @@
+SELECT
+	'AssetCapitalisation'||'-'||"o_custom_ANLB"."ID" AS "ID",
+	CAST("o_custom_ANLB"."CREATED ON" AS TIMESTAMP) AS "Time",
+    "o_custom_ANLB"."ID" AS ANLB
+FROM "o_custom_ANLB"
+
